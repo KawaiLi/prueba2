f687fa05b98df748d008262667982a6aaed0271b
KawaiLi <kawai.li@alum.udep.edu.pe>
2022-05-29 22:23:35 -0500

    Practicando codigo

--- prueba2.php
+++ prueba2.php
@@ -2,4 +2,18 @@
 echo 'HOLA KAWAI, ¿Cómo estás hoy?';
 echo '¿Ya comiste?';
 echo 'Buenos días Sensei';
+function saludar($Nombre) {
+  echo $Nombre
+}
+saludar('Perico');
+echo '</table>'
+for(int $i = 0, $i <= 5, $i++) {
+  echo  '<tr>';
+  for(int $i = 0, $i <= 10, $i++) {
+    echo  '<td>Hola '.$i.'-'.$y.' <td>';
+  }
+  echo  '<tr>';
+  }
+  echo  '</table>';
+
 ?>
